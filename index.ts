4545ffc117c87d808ce7e7077b9bca81d9ee91cb
Federico Rojas <federico.rojas.121@gmail.com>
2023-01-15 22:22:54 -0300

    server config done - cors & helmet basic config - root router ready

---
+++ index.ts
@@ -0,0 +1,18 @@
+import server from "./src/server";
+import { LogError, LogSuccess } from "./src/utils/logger";
+
+const express = require("express");
+const port: string | number = process.env.PORT || 8000;
+
+// * .env
+const dotenv = require("dotenv");
+dotenv.config();
+
+// * Execute SERVER
+server.listen(port, () => {
+     LogSuccess(`[SERVER ON]: Running in http://localhost:${port}/api`);
+});
+
+server.on("error", (error) => {
+     LogError(`[SERVER ERROR]: ${error}`);
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,36 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+// Security
+import cors from "cors";
+import helmet from "helmet";
+// TODO HTTPS
+
+// Root Router
+import rootRouter from "../routes";
+import { request } from "http";
+import { LogInfo } from "../utils/logger";
+
+// create Express app
+const server: Express = express();
+
+// * Define server to use "/api" amd use rootRouter from 'index.ts in routes
+// * http://localhost:PORT/api/...
+server.use("/api", rootRouter);
+
+// TODO Mongoose Connection
+
+// * Security Config
+server.use(helmet());
+server.use(cors());
+
+// * Content Type Config
+server.use(express.urlencoded({ extended: true, limit: "128mb" }));
+server.use(express.json({ limit: "128mb" }));
+
+// * Redirections Config
+// http:localhost:PORT/ --> http:localhost:PORT/api/
+server.get("/", (req: Request, res: Response) => {
+     LogInfo("redirect to /api");
+     res.redirect("/api");
+});
+
+export default server;
